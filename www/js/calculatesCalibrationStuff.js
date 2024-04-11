f8a6b324899e92a64ec83c2bd3c18811c90fb895
Ron Lawrence <ronlawrence3@gmail.com>
2024-04-11 05:45:46 -0600

    move stuff to make merging easier

--- www/js/calculatesCalibrationStuff.js
+++ www/js/calculatesCalibrationStuff.js
@@ -491,42 +491,6 @@ function scaleMeasurementsBasedOnTension(measurements, guess) {
   return newMeasurements
 }
 
-function measurementsChanged() {
-  try {
-    const calTextElement = document.getElementById("caldata");
-    let textValue = calTextElement.value;
-    if (textValue.indexOf('bl:')>0) {
-      textValue = JSON.stringify(eval(textValue));
-    }
-    let caldata = JSON.parse(textValue);
-    updateCalibrationSave(caldata);
-  } catch (err) {
-    console.error(err);
-  }
-}
-
-function updateCalibrationSave(caldata) {
-    SavedMeasurements = caldata;
-    calibrationTableUpdate();
-    document.querySelector('button#compute-sim-button').disabled = false;
-}
-
-function computeSim() {
-
-  clearCalCanvas();
-  findMaxFitness(SavedMeasurements);
-  results = document.querySelector("#messages").value
-  console.log(results);
-}
-
-
-//Deletes everything from the canvas
-function clearCalCanvas() {
-  const canvas = document.getElementById('CursorLayer');
-  const context = canvas.getContext('2d');
-  context.clearRect(0, 0, canvas.width, canvas.height);
-}
-
 function findMaxFitness(measurements) {
   if (window.computing) {
     console.log('Computation in progress, ignoring');
@@ -544,7 +508,7 @@ function findMaxFitness(measurements) {
   var messagesBox = document.querySelector('#messages');
 
   function loop() {
-
+    clearCalCanvas();
     currentGuess = computeLinesFitness(measurements, currentGuess);
 
     if (1/currentGuess.fitness > 1/bestGuess.fitness) {
@@ -562,7 +526,6 @@ function findMaxFitness(measurements) {
     }
 
     if (stagnantCounter < 100 && totalCounter < 200000) {
-      clearCanvas();
       requestAnimationFrame(loop);
     } else {
 
@@ -633,109 +596,3 @@ function findMaxFitness(measurements) {
 
 
 
-
-/**
- * Flips the y-coordinate of a point to account for the canvas having y at the top.
- * @param {number} y - The y-coordinate to flip.
- * @returns {number} - The flipped y-coordinate.
- */
-function flipY(y) {
-  var canvas = document.getElementById("CursorLayer");
-  return canvas.height - y;
-}
-
-function changeStrokeStyle(inputValue) {
-  const green = [0, 128, 0]; // RGB values for green
-  const red = [255, 0, 0]; // RGB values for red
-  const range = 60 - 20; // Range of input values
-  const increment = (red.map((value, index) => value - green[index])).map(value => value / range); // Increment for each RGB value
-
-  const color = green.map((value, index) => Math.round(value + increment[index] * (inputValue - 20))); // Calculate the color based on the input value
-
-  const canvas = document.getElementById("CursorLayer");
-  const ctx = canvas.getContext("2d");
-
-  ctx.strokeStyle = `rgb(${color[0]}, ${color[1]}, ${color[2]})`; // Set the strokeStyle to the calculated color
-}
-
-/**
- * Draws four lines on a canvas element and adds a circle at the end of each line.
- * @param {Object} line1 - An object containing the x and y coordinates of the beginning and end of the first line.
- * @param {Object} line2 - An object containing the x and y coordinates of the beginning and end of the second line.
- * @param {Object} line3 - An object containing the x and y coordinates of the beginning and end of the third line.
- * @param {Object} line4 - An object containing the x and y coordinates of the beginning and end of the fourth line.
- * @returns {void}
- */
-function drawLines(line1, line2, line3, line4, guess) {
-
-    //Compute the tensions in the upper two belts
-    //const { TL, TR } = calculateTensions(line1.xEnd, line1.yEnd, guess); //This assumes the ends are in the same place which they aren't at first
-
-    var canvas = document.getElementById("CursorLayer");
-    var ctx = canvas.getContext("2d");
-
-    // Set the stroke color to a lighter grey
-    ctx.strokeStyle = "#999";
-
-    // Draw the four lines
-    ctx.setLineDash([5, 5]);
-
-    //Top left line
-    ctx.beginPath();
-    ctx.moveTo(line1.xBegin / 4, flipY(line1.yBegin / 4));
-    ctx.lineTo(line1.xEnd / 4, flipY(line1.yEnd / 4));
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.arc(line1.xEnd / 4, flipY(line1.yEnd / 4), 2, 0, 2 * Math.PI);
-    ctx.fill();
-
-    //Top right line
-    ctx.beginPath();
-    ctx.moveTo(line2.xBegin / 4, flipY(line2.yBegin / 4));
-    ctx.lineTo(line2.xEnd / 4, flipY(line2.yEnd / 4));
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.arc(line2.xEnd / 4, flipY(line2.yEnd / 4), 2, 0, 2 * Math.PI);
-    ctx.fill();
-
-    ctx.beginPath();
-    ctx.moveTo(line3.xBegin / 4, flipY(line3.yBegin / 4));
-    ctx.lineTo(line3.xEnd / 4, flipY(line3.yEnd / 4));
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.arc(line3.xEnd / 4, flipY(line3.yEnd / 4), 2, 0, 2 * Math.PI);
-    ctx.fill();
-
-    ctx.beginPath();
-    ctx.moveTo(line4.xBegin / 4, flipY(line4.yBegin / 4));
-    ctx.lineTo(line4.xEnd / 4, flipY(line4.yEnd / 4));
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.arc(line4.xEnd / 4, flipY(line4.yEnd / 4), 2, 0, 2 * Math.PI);
-    ctx.fill();
-}
-
-function calibrationTableUpdate() {
-  const table = document.querySelector("#caltable");
-  if (table.rows.length) {
-    for (let i=table.rows.length; i>0;i--) {
-      table.deleteRow(0);
-    }
-  }
-  const headr = table.insertRow();
-  headr.insertCell(-1).innerText = "Action";
-  headr.insertCell(-1).innerText = "Top Left";
-  headr.insertCell(-1).innerText = "Top Right";
-  headr.insertCell(-1).innerText = "Bottom Right";
-  headr.insertCell(-1).innerText = "Bottom Left";
-  SavedMeasurements.forEach(
-    m => {
-      const row = table.insertRow();
-      row.insertCell(-1).innerText = "";
-      row.insertCell(-1).innerText = "" + m.tl;
-      row.insertCell(-1).innerText = "" + m.tr;
-      row.insertCell(-1).innerText = "" + m.br;
-      row.insertCell(-1).innerText = "" + m.bl;
-    }
-  )
-}
---
+++ www/js/calibrationDraw.js
@@ -0,0 +1,143 @@
+/**
+ * Functions for drawing on the cal details tab
+ */
+
+function measurementsChanged() {
+  try {
+    const calTextElement = document.getElementById("caldata");
+    let textValue = calTextElement.value;
+    if (textValue.indexOf('bl:')>0) {
+      textValue = JSON.stringify(eval(textValue));
+    }
+    let caldata = JSON.parse(textValue);
+    updateCalibrationSave(caldata);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function updateCalibrationSave(caldata) {
+    SavedMeasurements = caldata;
+    calibrationTableUpdate();
+    document.querySelector('button#compute-sim-button').disabled = false;
+}
+
+function computeSim() {
+  clearCalCanvas();
+  findMaxFitness(SavedMeasurements);
+  results = document.querySelector("#messages").value
+  console.log(results);
+}
+
+//Deletes everything from the canvas
+function clearCalCanvas() {
+  const canvas = document.getElementById('CursorLayer');
+  const context = canvas.getContext('2d');
+  context.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+/**
+ * Flips the y-coordinate of a point to account for the canvas having y at the top.
+ * @param {number} y - The y-coordinate to flip.
+ * @returns {number} - The flipped y-coordinate.
+ */
+function flipY(y) {
+  var canvas = document.getElementById("CursorLayer");
+  return canvas.height - y;
+}
+
+function changeStrokeStyle(inputValue) {
+  const green = [0, 128, 0]; // RGB values for green
+  const red = [255, 0, 0]; // RGB values for red
+  const range = 60 - 20; // Range of input values
+  const increment = (red.map((value, index) => value - green[index])).map(value => value / range); // Increment for each RGB value
+
+  const color = green.map((value, index) => Math.round(value + increment[index] * (inputValue - 20))); // Calculate the color based on the input value
+
+  const canvas = document.getElementById("CursorLayer");
+  const ctx = canvas.getContext("2d");
+
+  ctx.strokeStyle = `rgb(${color[0]}, ${color[1]}, ${color[2]})`; // Set the strokeStyle to the calculated color
+}
+
+/**
+ * Draws four lines on a canvas element and adds a circle at the end of each line.
+ * @param {Object} line1 - An object containing the x and y coordinates of the beginning and end of the first line.
+ * @param {Object} line2 - An object containing the x and y coordinates of the beginning and end of the second line.
+ * @param {Object} line3 - An object containing the x and y coordinates of the beginning and end of the third line.
+ * @param {Object} line4 - An object containing the x and y coordinates of the beginning and end of the fourth line.
+ * @returns {void}
+ */
+function drawLines(line1, line2, line3, line4, guess) {
+
+    //Compute the tensions in the upper two belts
+    //const { TL, TR } = calculateTensions(line1.xEnd, line1.yEnd, guess); //This assumes the ends are in the same place which they aren't at first
+
+    var canvas = document.getElementById("CursorLayer");
+    var ctx = canvas.getContext("2d");
+
+    // Set the stroke color to a lighter grey
+    ctx.strokeStyle = "#999";
+
+    // Draw the four lines
+    ctx.setLineDash([5, 5]);
+
+    //Top left line
+    ctx.beginPath();
+    ctx.moveTo(line1.xBegin / 4, flipY(line1.yBegin / 4));
+    ctx.lineTo(line1.xEnd / 4, flipY(line1.yEnd / 4));
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.arc(line1.xEnd / 4, flipY(line1.yEnd / 4), 2, 0, 2 * Math.PI);
+    ctx.fill();
+
+    //Top right line
+    ctx.beginPath();
+    ctx.moveTo(line2.xBegin / 4, flipY(line2.yBegin / 4));
+    ctx.lineTo(line2.xEnd / 4, flipY(line2.yEnd / 4));
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.arc(line2.xEnd / 4, flipY(line2.yEnd / 4), 2, 0, 2 * Math.PI);
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.moveTo(line3.xBegin / 4, flipY(line3.yBegin / 4));
+    ctx.lineTo(line3.xEnd / 4, flipY(line3.yEnd / 4));
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.arc(line3.xEnd / 4, flipY(line3.yEnd / 4), 2, 0, 2 * Math.PI);
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.moveTo(line4.xBegin / 4, flipY(line4.yBegin / 4));
+    ctx.lineTo(line4.xEnd / 4, flipY(line4.yEnd / 4));
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.arc(line4.xEnd / 4, flipY(line4.yEnd / 4), 2, 0, 2 * Math.PI);
+    ctx.fill();
+}
+
+function calibrationTableUpdate() {
+  const table = document.querySelector("#caltable");
+  if (table.rows.length) {
+    for (let i=table.rows.length; i>0;i--) {
+      table.deleteRow(0);
+    }
+  }
+  const headr = table.insertRow();
+  headr.insertCell(-1).innerText = "Action";
+  headr.insertCell(-1).innerText = "Top Left";
+  headr.insertCell(-1).innerText = "Top Right";
+  headr.insertCell(-1).innerText = "Bottom Right";
+  headr.insertCell(-1).innerText = "Bottom Left";
+  SavedMeasurements.forEach(
+    m => {
+      const row = table.insertRow();
+      row.insertCell(-1).innerText = "";
+      row.insertCell(-1).innerText = "" + m.tl;
+      row.insertCell(-1).innerText = "" + m.tr;
+      row.insertCell(-1).innerText = "" + m.br;
+      row.insertCell(-1).innerText = "" + m.bl;
+    }
+  )
+}
